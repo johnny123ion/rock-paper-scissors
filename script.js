9af17ff230e0abaca7e1a0417d15ed2cb432fbd4
Ion Daraban <116725504+johnny123ion@users.noreply.github.com>
2023-01-17 17:15:43 +0200

    Add functions getPlayerSelection() and playRound()
    The function getPlayerSelection() returns user's choice between 'Rock', Paper' or
    'Scissors'. The function playRound() compares the computer's choice with the player's
    choice and returns the result as a string

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
 function getComputerChoice() {
   let myArray = ['Rock', 'Paper', 'Scissors'];
   let pcChoice = Math.floor(Math.random() * myArray.length);
-  let finalChoice;
+  let finalPCChoice;
   if (pcChoice === 0) {
-    finalChoice = myArray[0];
+    finalPCChoice = myArray[0];
   } else if (pcChoice === 1) {
-    finalChoice = myArray[1];
+    finalPCChoice = myArray[1];
   } else {
-    finalChoice = myArray[2];
+    finalPCChoice = myArray[2];
   }
-  return finalChoice;
+  return finalPCChoice;
 };
 
+
+function getPlayerSelection() {
+  let playerChoice = prompt('Rock, Paper or Scissors?');
+  let finalPlayerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+  return finalPlayerChoice;
+};
+
+
+function playRound() {
+
+  let pcChoiceVar = getComputerChoice();
+  let playerChoiceVar = getPlayerSelection();
+  let result;
+
+  if (pcChoiceVar === 'Rock' && playerChoiceVar === 'Paper') {
+    result = 'You win! Paper beats Rock';
+
+  } else if (pcChoiceVar === 'Rock' && playerChoiceVar === 'Scissors') {
+    result = 'You lose! Rock beats Scissors';
+
+  } else if (pcChoiceVar === 'Paper' && playerChoiceVar === 'Rock') {
+    result = 'You lose! Paper beats Rock';
+
+  } else if (pcChoiceVar === 'Paper' && playerChoiceVar === 'Scissors') {
+    result = 'You win! Scissors beats Paper';
+
+  } else if (pcChoiceVar === 'Scissors' && playerChoiceVar === 'Rock') {
+    result = 'You win! Rock beats Scissors';
+
+  } else if (pcChoiceVar === 'Scissors' && playerChoiceVar === 'Paper') {
+    result = 'You lose! Scissors beats Paper';
+
+  } else if (pcChoiceVar === playerChoiceVar) {
+    result = 'Tie!';
+
+  } else {
+    result = "You didn't enter 'Rock', 'Paper' or 'Scissors'";
+  
+  };
+  return result;
+};
